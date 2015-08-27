d13c0ba0263bb729947dfe5bfd0cc41a9a5d55e4
kdemerath <kdemerath@gmx.de>
2015-08-27 15:30:04 +0200

    some error handling
    added some error handling
    added start screen
    added card for
    - missing settings
    - server not available
    - no tasks
    removed unneccessary

--- src/app.js
+++ src/app.js
@@ -5,16 +5,13 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
 var Settings = require('settings');
+var ajax = require('ajax');
 
 // habitica API constants
 var habiticaBaseUrl = 'https://habitica.com/api/v2';
 var habiticaStatus = '/status'; //Returns the status of the server (up or down). Does not require authentication.
 var habiticaGetUserTasksUrl = '/user/tasks';
-
-// AJAX connection
-var ajax = require('ajax');
 
 // Set a configurable
 Settings.config(
@@ -36,109 +33,124 @@ Settings.config(
 );
 
 // check habitica status
-checkHabiticaStatus();
-
-// get all tasks
-var allTasks = [];
-getUserTasks();
-
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
-});
-
-main.show();
-
-main.on('click', 'up', function(e) {
-  var sectionHabits = {
-    title: 'Habits',
-    items: []
-  };
-  var sectionDailies = {
-    title: 'Dailies',
-    items: []
-  };
-  var sectionToDos = {
-    title: 'To-Dos',
-    items: []
-  };
-  
-  if(!allTasks){console.log('allTasks is undefined');} else {
-  var allTasksPrep = allTasks.map(
-    function(x) {
-      x.title = x.text;
-      return x;
-    }
-  );
-  sectionHabits.items = allTasksPrep.filter(
-    function(x){
-      return x.type == 'habit';
-    }
-  );
-  sectionDailies.items = allTasksPrep.filter(
-    function(x){
-      return x.type == 'daily';
-    }
-  );
-  sectionToDos.items = allTasksPrep.filter(
-    function(x){
-      return x.type == 'todo';
-    }
-  );
-  }
-  
-  var menu = new UI.Menu();
-  menu.section(1, sectionHabits);
-  menu.section(2, sectionDailies);
-  menu.section(3, sectionToDos);
-  
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-    scoreTaskUp(e.item);
+if (!checkHabiticaStatus) {
+  var cardNoServer = new UI.Card({
+    title: 'Server unavaiable',
+    body: 'habitica Server is not available. Please restart.'
   });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
+  cardNoServer.show();
+} else if(!Settings.option('userId') || !Settings.option('apiToken') || Settings.option('userId') === '' || Settings.option('apiToken') === '') {
+  var cardSettingsIncomplete = new UI.Card({
+    title: 'Settings incomplete',
+    body: 'Please enter your credentials in the settings.'
   });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
+  cardSettingsIncomplete.show();
+} else {
+  
+  // get all tasks
+  var allTasks = [];
+  getUserTasks();
+  
+  // create start screen
+  var main = new UI.Card({
+    title: 'habitica',
+    icon: 'images/menu_icon.png',
+    subtitle: 'Tasks',
+    body: 'Press down to view your tasks.'
   });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+  main.show();
+  
+  main.on('click', 'down', function(e) {
+    if (!allTasks) {
+      var cardNoTasks = new UI.Card({
+        title: 'No tasks',
+        body: 'Please retry.'
+      });
+      cardNoTasks.show();
+    } else {
+      // create tasks menu
+      var menuAllTasks = new UI.Menu();
+      menuAllTasks = createTasksMenu();
+      menuAllTasks.show();
+    }
+  });
+}
 
 function checkHabiticaStatus() {
+  var serverIsUp = false;
   ajax(
     {
       url: habiticaBaseUrl + habiticaStatus,
-      type: 'json'
+      type: 'json',
+      async: 'false'
     },
     function(data, status, request) {
       console.log('Habitica Server Status: ' + data.status);
+      if (data.status == 'up') {serverIsUp = true;}
     },
     function(error, status, request) {
       console.log('The ajax request failed: ' + error);
     }
   );
+  return serverIsUp;
 }
+
+function createTasksMenu() {
+    var menu = new UI.Menu();
+    var sectionHabits = {
+      title: 'Habits',
+      items: []
+    };
+    var sectionDailies = {
+      title: 'Dailies',
+      items: []
+    };
+    var sectionToDos = {
+      title: 'To-Dos',
+      items: []
+    };
+    
+    if(!allTasks){
+      console.log('allTasks is undefined');
+    } else {
+      var allTasksPrep = allTasks.slice();
+      console.log('allTasksPrep lenght ' + allTasksPrep.lenght);
+      allTasksPrep = allTasksPrep.map(
+        function(x) {
+          x.title = x.text;
+          return x;
+        }
+      );
+      console.log('allTasksPrep lenght ' + allTasksPrep.lenght);
+      sectionHabits.items = allTasksPrep.filter(
+        function(x){
+          return x.type == 'habit';
+        }
+      ).slice();
+      console.log('sectionHabits.items lenght ' + sectionHabits.items.lenght);
+      sectionDailies.items = allTasksPrep.filter(
+        function(x){
+          return x.type == 'daily';
+        }
+      ).slice();
+      sectionToDos.items = allTasksPrep.filter(
+        function(x){
+          return x.type == 'todo';
+        }
+      ).slice();
+      }
+    
+    menu.section(1, sectionHabits);
+    menu.section(2, sectionDailies);
+    menu.section(3, sectionToDos);
+    
+    menu.on('select', function(e) {
+      console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+      console.log('The item is titled "' + e.item.title + '"');
+      scoreTaskUp(e.item);
+    });
+    return menu;
+  }
 
 function getUserTasks() {
   ajax(
